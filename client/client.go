010d5ce6e1485a26bb0d5a1acf90179a36bc45a8
Hubler Jens <jens.hubler@bluewin.ch>
2024-02-22 13:17:33 +0100

    simple postgres client

--- client/client.go
+++ client/client.go
@@ -5,7 +5,9 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+
 	_ "github.com/lib/pq"
+
 	//"strings"
 	//"cloud.google.com/go/spanner"
 	"github.com/golang/glog"
@@ -19,44 +21,55 @@ import (
 )
 
 var (
-	hashStorage      = flag.String("hashStorage", "", "Storage used for computed hashes, can have one of the two values: postgres, cloudspanner")
-	cacheDir         = flag.String("cache_dir", "/tmp/", "Path to cache dir used to store local cache.")
-	spannerDBPath    = flag.String("spanner_db_path", "", "Path to spanner DB.")
+	hashStorage   = flag.String("hashStorage", "", "Storage used for computed hashes, can have one of the two values: postgres, cloudspanner")
+	cacheDir      = flag.String("cache_dir", "/tmp/", "Path to cache dir used to store local cache.")
+	spannerDBPath = flag.String("spanner_db_path", "", "Path to spanner DB.")
 
 	// Postgres DB flags
 	postgresHost     = flag.String("postgres_host", "localhost", "PostgreSQL instance address.")
 	postgresPort     = flag.Int("postgres_port", 5432, "PostgresSQL instance port.")
 	postgresUser     = flag.String("postgres_user", "hashr", "PostgresSQL user.")
 	postgresPassword = flag.String("postgres_password", "hashr", "PostgresSQL password.")
-	postgresDBName   = flag.String("postgres_db", "hashr", "PostgresSQL database.")	
+	postgresDBName   = flag.String("postgres_db", "hashr", "PostgresSQL database.")
+
+	// Mode of retrieval
+	get = flag.String("get_sample", "", "Sample to retrieve based on sha256 hash")
 )
 
-func main(){
-	//ctx := context.Background()
+func main() {
 	flag.Parse()
 	var client *postgresClient.Client
 	switch *hashStorage {
-		case postgresClient.Name:
-			psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	case postgresClient.Name:
+		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 			*postgresHost, *postgresPort, *postgresUser, *postgresPassword, *postgresDBName)
 
-			db, err := sql.Open("postgres", psqlInfo)
-			if err != nil {
-				glog.Exitf("Error initializing Postgres client: %v", err)
-			}
-			defer db.Close()
+		db, err := sql.Open("postgres", psqlInfo)
+		if err != nil {
+			glog.Exitf("Error initializing Postgres client: %v", err)
+		}
+		defer db.Close()
 
-			client, err = postgresClient.NewClient(db)
-			if err != nil {
-				glog.Exitf("Error initializing Postgres storage: %v", err)
-			}
-		default:
-			glog.Exit("hashStorage flag needs to have one of the two values: postgres, cloudspanner")
-		
+		client, err = postgresClient.NewClient(db)
+		if err != nil {
+			glog.Exitf("Error initializing Postgres storage: %v", err)
+		}
+	default:
+		glog.Exit("hashStorage flag needs to have one of the two values: postgres, cloudspanner")
+
 	}
-	samples, err := client.GetSamples()
-	if err != nil {
-		glog.Exitf("Error retriving samples: %v", err)
+	if *get == "" {
+		samples, err := client.GetSamples()
+		if err != nil {
+			glog.Exitf("Error retriving samples: %v", err)
+		}
+		fmt.Println(samples)
+	} else {
+
+		sample, err := client.GetSample(*get)
+		if err != nil {
+			glog.Exitf("Error retriving samples: %v", err)
+		}
+		fmt.Println(*sample)
 	}
-	fmt.Println(samples)
 }
--- client/postgres/postgres.go
+++ client/postgres/postgres.go
@@ -16,17 +16,10 @@
 package postgres
 
 import (
-//	"bytes"
-//	"context"
 	"database/sql"
 	"fmt"
-//	"net/http"
-//	"os"
-//	"os/exec"
-//	"strings"
-//	"github.com/golang/glog"
-//	"github.com/google/hashr/common"
-_	"github.com/lib/pq"
+
+	_ "github.com/lib/pq"
 )
 
 const (
@@ -36,15 +29,15 @@ const (
 
 // Client is an instance of Postgres Client.
 type Client struct {
-	sqlDB          *sql.DB
+	sqlDB *sql.DB
 }
 
-// Client is an instance of Postgres Client.
+// Sample representing samples as stored in DB.
 type Sample struct {
-	sha256		string
-	mimetype	string
+	sha256      string
+	mimetype    string
 	file_output string
-	size		int
+	size        int
 }
 
 // Name returns Client name.
@@ -52,99 +45,33 @@ func (c *Client) Name() string {
 	return Name
 }
 
-// NewClient creates new Postregre Client and all the necessary tables, if they don't exist.
+// NewClient creates new Postregre Client.
 func NewClient(sqlDB *sql.DB) (*Client, error) {
 	// Check if the "samples" table exists.
-	exists, err := tableExists(sqlDB, "samples")
+	_, err := tableExists(sqlDB, "samples")
 	if err != nil {
 		return nil, fmt.Errorf("error while checking if samples table exists: %v", err)
 	}
 
-	if !exists {
-		sql := `CREATE TABLE samples (
-				sha256 VARCHAR(100)  PRIMARY KEY,
-				mimetype text,
-				file_output  text,
-				size INT
-		  )`
-		_, err = sqlDB.Exec(sql)
-		if err != nil {
-			return nil, fmt.Errorf("error while creating samples table: %v", err)
-		}
-	}
-
 	// Check if the "payloads" table exists.
-	exists, err = tableExists(sqlDB, "payloads")
+	_, err = tableExists(sqlDB, "payloads")
 	if err != nil {
 		return nil, fmt.Errorf("error while checking if payloads table exists: %v", err)
 	}
 
-	if !exists {
-		sql := `CREATE TABLE payloads (
-				sha256 VARCHAR(100)  PRIMARY KEY,
-				payload bytea
-		  )`
-		_, err = sqlDB.Exec(sql)
-		if err != nil {
-			return nil, fmt.Errorf("error while creating payloads table: %v", err)
-		}
-	}
-
 	// Check if the "sources" table exists.
-	exists, err = tableExists(sqlDB, "sources")
+	_, err = tableExists(sqlDB, "sources")
 	if err != nil {
 		return nil, fmt.Errorf("error while checking if sources table exists: %v", err)
 	}
 
-	if !exists {
-		sql := `CREATE TABLE sources (
-			sha256 VARCHAR(100)  PRIMARY KEY,
-			sourceID  text[],
-			sourcePath  text,
-			sourceDescription text,
-			repoName text,
-			repoPath text
-		  )`
-		_, err = sqlDB.Exec(sql)
-		if err != nil {
-			return nil, fmt.Errorf("error while creating sources table: %v", err)
-		}
-	}
-
 	// Check if the "samples_sources" table exists.
-	exists, err = tableExists(sqlDB, "samples_sources")
+	_, err = tableExists(sqlDB, "samples_sources")
 	if err != nil {
 		return nil, fmt.Errorf("error while checking if samples_sources table exists: %v", err)
 	}
 
-	if !exists {
-		sql := `CREATE TABLE samples_sources (
-			sample_sha256 VARCHAR(100) REFERENCES samples(sha256) NOT NULL,
-			source_sha256 VARCHAR(100) REFERENCES sources(sha256) NOT NULL,
-			sample_paths text[],
-			PRIMARY KEY (sample_sha256, source_sha256)
-		  )`
-		_, err = sqlDB.Exec(sql)
-		if err != nil {
-			return nil, fmt.Errorf("error while creating samples_sources table: %v", err)
-		}
-	}
-
 	return &Client{sqlDB: sqlDB}, nil
-}
-
-func (c *Client) sampleExists(sha256 string) (bool, error) {
-	sqlStatement := `SELECT sha256 FROM samples WHERE sha256=$1;`
-	var quickSha256 string
-	row := c.sqlDB.QueryRow(sqlStatement, sha256)
-	switch err := row.Scan(&quickSha256); err {
-	case sql.ErrNoRows:
-		return false, nil
-	case nil:
-		return true, nil
-	default:
-		return false, err
-	}
 }
 
 func (c *Client) GetSamples() ([]Sample, error) {
@@ -156,18 +83,14 @@ func (c *Client) GetSamples() ([]Sample, error) {
 	if exists {
 		var sql string
 		sql = `SELECT * FROM samples;`
-
 		rows, err := c.sqlDB.Query(sql)
-
 		if err != nil {
 			return nil, err
 		}
-
-		defer rows.Close() 
-		
-		for rows.Next(){
+		defer rows.Close()
+		for rows.Next() {
 			var sample Sample
-			err := rows.Scan(&sample.sha256, &sample.mimetype, &sample.file_output, &sample.size);
+			err := rows.Scan(&sample.sha256, &sample.mimetype, &sample.file_output, &sample.size)
 			if err != nil {
 				return nil, err
 			}
@@ -176,8 +99,22 @@ func (c *Client) GetSamples() ([]Sample, error) {
 	} else {
 		return nil, fmt.Errorf("Table samples does not exist")
 	}
-	
 	return samples, nil
+}
+
+func (c *Client) GetSample(sha256 string) (*Sample, error) {
+	exists, err := tableExists(c.sqlDB, "samples")
+	if err != nil {
+		return nil, err
+	}
+	var sample Sample
+	if exists {
+		sqlStatement := `SELECT * FROM samples WHERE sha256=$1;`
+		row := c.sqlDB.QueryRow(sqlStatement, sha256)
+		err := row.Scan(&sample.sha256, &sample.mimetype, &sample.file_output, &sample.size)
+		return &sample, err
+	}
+	return nil, fmt.Errorf("Table samples does not exist")
 }
 
 func (c *Client) sourceExists(sha256 string) (bool, error) {
